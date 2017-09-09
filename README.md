929e08ae768e6f97e86f712553822458f948231d
sqliang <1053196988@qq.com>
2017-09-10 00:18:52 +0800

    add the python-learn

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pythonRep
 Python相关学习和项目代码库
+
+## python-learn
+python基本知识学习代码库
---
+++ python-learn/onelearn.py
@@ -0,0 +1,24 @@
+# learn langurage
+print("hello world");
+
+if True:
+    print('Hello, true');
+    print("haha");
+else:
+    print('Hello, false');
+# 多行语句
+itemOne = 1;
+itemTwo = 2;
+itemThree = 3;
+total = itemOne + \
+        itemTwo + \
+        itemThree
+print(total);
+
+# [],{},()中的多行语句不需要使用 \
+hello = [
+    'item_one','item_two',
+    'item_three',
+    'item_four'
+]
+print(hello);
